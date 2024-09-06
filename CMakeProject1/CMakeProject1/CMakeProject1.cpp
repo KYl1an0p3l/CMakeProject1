202ab543486f6343e21e08e60a6049622d5eccc8
SansStylo <112880475+KYl1an0p3l@users.noreply.github.com>
2024-09-06 16:44:52 +0200

    Création

---
+++ CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeProject1.cpp : définit le point d'entrée de l'application.
+//
+
+#include "CMakeProject1.h"
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	printf("Hello World\n");
+	return EXIT_SUCCESS;
+}
